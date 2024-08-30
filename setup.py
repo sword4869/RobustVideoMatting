c2eefcc800432d9e33446d55608c92eb3e6d202b
sword4869 <1084662708@qq.com>
2024-08-30 09:17:25 +0800

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/sword4869/RobustVideoMatting",
-    python_requires=">=3.10",
     install_requires=[
         'torch',
         'torchvision',
